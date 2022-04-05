7c2b24e6231e828dea30e1aed55dff556713ea28
Prajwal-Sharma09 <prajwalsharmawqu09@gmail.com>
2022-04-05 14:04:12 +0545

    small changes in index.js with react 18 version

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from "react";
-import ReactDom from "react-dom";
+import * as ReactDOMClient from "react-dom/client";
 import "./index.css";
 
 import App from "./App";
 
-ReactDom.render(<App />, document.getElementById("root"));
+const container = document.getElementById("root");
+const root = ReactDOMClient.createRoot(container);
+root.render(<App />);

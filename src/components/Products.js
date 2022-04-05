8a156355066ea9b40fa8e9c3b8f704581d3ae42f
Prajwal-Sharma09 <prajwalsharmawqu09@gmail.com>
2022-04-05 19:54:07 +0545

    testing with api and it works

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Products = () => {
-  return <div>Products</div>;
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const res = await fetch(
+        "https://electronic-ecommerce.herokuapp.com/api/v1/product"
+      );
+      const response = await res.json();
+      console.log(response.data.product);
+      setProducts(response.data.product);
+    };
+    fetchProducts();
+  }, []);
+  return (
+    <div>
+      {products.map((product) => (
+        <div>
+          {" "}
+          <h5>{product.name}</h5>
+          <h5>{product.price}</h5>
+          <h5>{product.stock}</h5>
+          <h5>{product.category[0] + " " + product.category[1]}</h5>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Products;

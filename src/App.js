d2e24ace3ed3470eed13a4a0c202c8cdcfcfb3b2
Prajwal-Sharma09 <prajwalsharmawqu09@gmail.com>
2022-04-06 19:33:19 +0545

    basic setup  with redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store/store";
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
 import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" exact element={<Home />}></Route>
-        <Route path="/cart" element={<Cart />}></Route>
-      </Routes>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Home />}></Route>
+          <Route path="/cart" element={<Cart />}></Route>
+        </Routes>
+      </Router>
+    </Provider>
   );
 };
 
